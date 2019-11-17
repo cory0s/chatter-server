03832699a3247cfe501094bee3bb5b17644f7116
Cory Henderson <chenderson19899@gmail.com>
2019-11-16 16:29:28 -0800

    connect/disconnect

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
-server.listen(80, () => {
-    console.log('Server up on port 80');
+server.listen(8000, () => {
+    console.log('Server up on port 8000');
 });
 
-io.on('connection', (client) => {
-    console.log('Socket connected:', client);
-    client.on('join', { hello: world });
+let socketPool = [];
+
+io.on('connection', client => {
+    console.log('Socket connected!', client.id);
+    socketPool.push(client.id);
+    client.emit('join', client.id);
+
+
+});
+
+io.on('disconnect', client => {
+    console.log('Socket disconnected');
 })
