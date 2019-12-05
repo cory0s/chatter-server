90c6afdcf8b83360696cbed3e2ad1b3487168ea5
Cory Henderson <chenderson19899@gmail.com>
2019-12-05 12:24:51 -0800

    leave room msg

--- server.js
+++ server.js
@@ -23,19 +23,27 @@ io.on('connection', (socket) => {
         
         socket.join(user.room);
 
-        // callback(); //Callback at front-end (if statement?)
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback(); //Callback at front-end (if statement?)
     });
     
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
         callback(); //Do something after message is sent on FE
     })
 
     socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
         console.log('Someone left...', socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`});
+        }
     })
 });
 
